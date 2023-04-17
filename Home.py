014b4c3a4ee25ebc9cec933e8262b6222cedcaf5
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 23:07:00 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -22,8 +22,8 @@ st.write('<style> \
             .caption, .subheader, .markdown {font-family: Georgia, serif; font-size: 24px !important;} \
           </style>', unsafe_allow_html=True)
 
-st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.",font=("Georgia", 20))
-st.subheader('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.',font=("Georgia", 20))
+st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
+st.subheader('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
 st.markdown("# Data Summary")
 
 
