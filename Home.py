ed6d5adfe517d69fa020b1ca6c6e948e5ec597ba
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 23:08:58 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ image_path=os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "pexel
 df=pd.read_csv(filepath)
 
 
-st.markdown("<h1 style='text-align: center; font-size: 30px;font-family: Georgia,serif;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 24px;font-family: Georgia,serif;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
 
 image= Image.open(image_path)
 st.image(image, use_column_width= True)
@@ -22,8 +22,8 @@ st.write('<style> \
             .caption, .subheader, .markdown {font-family: Georgia, serif; font-size: 24px !important;} \
           </style>', unsafe_allow_html=True)
 
-st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
-st.subheader('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
+st.caption("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
+st.caption('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
 st.markdown("# Data Summary")
 
 
