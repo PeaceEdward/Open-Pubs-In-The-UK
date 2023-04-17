904fbe5f9c9e2d861c0fdfe2e10582537e5e5dcc
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 21:32:42 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,14 +2,19 @@ import os
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 
 filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
+image_path=os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "pexels-chan-walrus-941864.jpg"))
+
 df=pd.read_csv(filepath)
 
 
 st.markdown("<h1 style='text-align: center; font-size: 36px;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
 
+image= Image.open(image_path)
+st.image(image, use_column_width= True)
 
 st.write('<style> .subheader{ max-width: 100%; }</style>', unsafe_allow_html=True)
 st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
