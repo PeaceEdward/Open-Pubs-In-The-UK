be1836fc283f413f681d3bf6a161b3caa44aea34
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 19:16:02 +0100

    Update Pub Locations.py

--- pages/Pub Locations.py
+++ pages/Pub Locations.py
@@ -1,13 +1,14 @@
 import os
 import streamlit as st
 import numpy as np
+import pandas as pd
 import folium
 from geopy.distance import geodesic
 from folium.plugins import MarkerCluster
 
-df = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
+filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
 
-
+pd.read_csv(filepath)
 
 
 
@@ -25,13 +26,13 @@ else:
 # Display pubs on a map
 if len(pubs) > 0:
     # Get center of the map as the average of the latitudes and longitudes of the pubs
-    center_lat = pubs["Latitude"].mean()
-    center_lon = pubs["Longitude"].mean()
+    center_lat = pubs["latitude"].mean()
+    center_lon = pubs["longitude"].mean()
     # Create a Folium map centered on the location and add the pubs as markers
     m = folium.Map(location=[center_lat, center_lon], zoom_start=13)
     marker_cluster = MarkerCluster().add_to(m)
     for index, row in pubs.iterrows():
-        folium.Marker(location=[row["Latitude"], row["Longitude"]], popup=row["Name"]).add_to(marker_cluster)
+        folium.Marker(location=[row["latitude"], row["longitude"]], popup=row["name"]).add_to(marker_cluster)
     st.write(m)
 else:
     st.write("No pubs found in the specified location.")
