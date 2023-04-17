efda3aa79971a42f283c82a023d371635e7747cd
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 18:58:59 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,15 +1,17 @@
 import os
 import streamlit as st
 import numpy as np
+import pandas as pd
 import folium
 from geopy.distance import geodesic
 from folium.plugins import MarkerCluster
 
 
 
-df = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
+filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
+pd.read_csv(filepath)
 
 st.markdown("# Summary Statistics")
 st.write("There are currently {} pubs in the dataset.".format(len(df)))
-st.write("The average latitude is {} and the average longitude is {}.".format(df["Latitude"].mean(), df["Longitude"].mean()))
-st.write("The pub with the longest name is '{}'.".format(df.loc[df["Name"].str.len().idxmax(), "Name"]))
+st.write("The average latitude is {} and the average longitude is {}.".format(df["latitude"].mean(), df["longitude"].mean()))
+st.write("The pub with the longest name is '{}'.".format(df.loc[df["name"].str.len().idxmax(), "name"]))
