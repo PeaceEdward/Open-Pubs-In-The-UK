d51d3a0859ead2f2bdd29f7fec8b05762a16b186
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 21:10:06 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,9 +7,9 @@ import pandas as pd
 filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
 df=pd.read_csv(filepath)
 
-st.title('OPEN PUBS IN THE UNITED KINGDOM')
+st.subtitle('OPEN PUBS IN THE UNITED KINGDOM')
 
-st.text("Pubs are establishments that serve alcoholic beverages, such as beer, wine, and spirits,\n along with snacks and other simple meals.\n Pubs are a popular social gathering place in the UK and are deeply ingrained in British culture. \n They are fun hangout spots for you, your friends and family with a lively atmosphere.")
-
+st.caption("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,\n along with snacks and other simple meals.\n They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
+st.caption('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.)
 st.markdown("# Summary Statistics")
 st.write("There are currently {} pubs in the dataset.".format(len(df)))
