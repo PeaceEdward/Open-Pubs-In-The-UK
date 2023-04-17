a1c1fef3262e3c73c109e9c0177bec77fb5fb953
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 21:21:06 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,11 +10,11 @@ df=pd.read_csv(filepath)
 #st.header('OPEN PUBS IN THE UNITED KINGDOM')
 
 
-st.markdown("<h1 style='text-align: center; font-size: 36px;'>OPEN PUBS IN THE UNITED KINGDOM</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 36px;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
 
 
-st.write('<style> .caption{ max-width: 100%; }</style>', unsafe_allow_html=True)
-st.caption("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,\n along with snacks and other simple meals.\n They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
+st.write('<style> .subheader{ max-width: 100%; }</style>', unsafe_allow_html=True)
+st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,\n along with snacks and other simple meals.\n They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
 st.caption('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
 st.markdown("# Summary Statistics")
 st.write("There are currently {} pubs in the dataset.".format(len(df)))
