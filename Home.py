c1b7d0841b74e2e162a1b575116d86ea6f90800a
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 21:13:24 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,6 +11,6 @@ st.subtitle('OPEN PUBS IN THE UNITED KINGDOM')
 
 st.write('<style> .caption{ max-width: 100%; }</style>', unsafe_allow_html=True)
 st.caption("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,\n along with snacks and other simple meals.\n They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
-st.caption('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.)
+st.caption('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
 st.markdown("# Summary Statistics")
 st.write("There are currently {} pubs in the dataset.".format(len(df)))
