736af5e0eb66c9a65557e014d5fa7b1a00d48180
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 22:48:19 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,12 +11,12 @@ image_path=os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "pexel
 df=pd.read_csv(filepath)
 
 
-st.markdown("<h1 style='text-align: center; font-size: 36px;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-size: 36px;font-family: Georgia,serif;'>OPEN PUBS IN THE UK</h1>", unsafe_allow_html=True)
 
 image= Image.open(image_path)
 st.image(image, use_column_width= True)
 
-st.write('<style> .subheader{ max-width: 100%; display: flex; text-align: center; }</style>', unsafe_allow_html=True)
+st.write('<style> .subheader{ max-width: 100%; display: flex; text-align: center;font-family: Georgia,serif; font-size: 24px; }</style>', unsafe_allow_html=True)
 
 st.subheader("Pubs are establishments that serve alcoholic beverages,such as beer, wine, and spirits,along with snacks and other simple meals. They are a popular social gathering places and fun hangout spots for you, your friends and family with a lively atmosphere.")
 st.subheader('This app helps you browse and explore pubs in the United Kingdom and find those nearest to you.')
