52fcbaef49725b5a532f6c6d01b9ba0f23196e77
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-04-17 21:06:24 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,16 +2,15 @@ import os
 import streamlit as st
 import numpy as np
 import pandas as pd
-import folium
-from geopy.distance import geodesic
-from folium.plugins import MarkerCluster
-
+import seaborn as sns
 
 
 filepath = os.path.abspath(os.path.join(os.getcwd(), "resources", "data", "uk_pubs.csv"))
 df=pd.read_csv(filepath)
 
+st.title('OPEN PUBS IN THE UNITED KINGDOM')
+
+st.text("Pubs are establishments that serve alcoholic beverages, such as beer, wine, and spirits,\n along with snacks and other simple meals.\n Pubs are a popular social gathering place in the UK and are deeply ingrained in British culture. \n They are fun hangout spots for you, your friends and family with a lively atmosphere.")
+
 st.markdown("# Summary Statistics")
 st.write("There are currently {} pubs in the dataset.".format(len(df)))
-st.write("The average latitude is {} and the average longitude is {}.".format(df["latitude"].mean(), df["longitude"].mean()))
-st.write("The pub with the longest name is '{}'.".format(df.loc[df["name"].str.len().idxmax(), "name"]))
